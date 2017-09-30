69241f3952ba4663e0d76e2d845d7db8d9d43e6c
Dakara <chad.meadows@gmail.com>
2017-09-30 11:14:29 -0400

    updates

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Navigator
 
-Additional editor navigation commands
+Additional editor navigation commands.
+
+Currently this is mostly a personal project, but might expand later to something more interesting.
 
 
 
--- src/extension.ts
+++ src/extension.ts
@@ -3,53 +3,31 @@ import * as vscode from 'vscode';
 import * as edit from 'vscode-extension-common'
 
 export function activate(context: vscode.ExtensionContext) {
-    let disposable = vscode.commands.registerCommand('navigator.skipSpaceUp', () => {
-        const textEditor = vscode.window.activeTextEditor;
-        const selection = textEditor.selection;
-        const line = edit.nextPositionUpWithTextRightOfPosition(textEditor, selection.anchor.line, selection.anchor.character);
-        textEditor.selection = new vscode.Selection(line.line, line.character, line.line, line.character);
-        textEditor.revealRange(textEditor.selection, vscode.TextEditorRevealType.Default);        
-    });
-    context.subscriptions.push(disposable);
-    
-    disposable = vscode.commands.registerCommand('navigator.skipSpaceDown', () => {
-        const textEditor = vscode.window.activeTextEditor;
-        const selection = textEditor.selection;
-        const line = edit.nextPositionDownWithTextRightOfPosition(textEditor, selection.anchor.line, selection.anchor.character);
-        textEditor.selection = new vscode.Selection(line.line, line.character, line.line, line.character);
-        textEditor.revealRange(textEditor.selection, vscode.TextEditorRevealType.Default);        
-    });
-    context.subscriptions.push(disposable);
-
-    disposable = vscode.commands.registerCommand('navigator.contiguousSpaceUp', () => {
-        const textEditor = vscode.window.activeTextEditor;
-        const selection = textEditor.selection;        
-    });
-    context.subscriptions.push(disposable);
-
-    disposable = vscode.commands.registerCommand('navigator.contiguousSpaceDown', () => {
-        const textEditor = vscode.window.activeTextEditor;
-        const selection = textEditor.selection;        
-    });
-    context.subscriptions.push(disposable);
- 
-    disposable = vscode.commands.registerCommand('navigator.fixedSpaceUp', () => {
-        Array.from(Array(10).keys()).forEach(() => vscode.commands.executeCommand("cursorUp"));
+    let disposable = vscode.commands.registerCommand('navigator.fixedSpaceUp', () => {
+        Array.from(Array(5).keys()).forEach(() => vscode.commands.executeCommand("cursorUp"));
     });
     context.subscriptions.push(disposable);
 
     disposable = vscode.commands.registerCommand('navigator.fixedSpaceDown', () => {
-        Array.from(Array(10).keys()).forEach(() => vscode.commands.executeCommand("cursorDown"));        
+        Array.from(Array(5).keys()).forEach(() => vscode.commands.executeCommand("cursorDown"));        
     });
     context.subscriptions.push(disposable);
 
-    disposable = vscode.commands.registerCommand('navigator.fixedSpaceRight', () => {
-        Array.from(Array(10).keys()).forEach(() => vscode.commands.executeCommand("cursorRight"));
+    disposable = vscode.commands.registerCommand('navigator.wordRightEnd', () => {
+        const textEditor = vscode.window.activeTextEditor;
+        const selection = textEditor.selection;
+        const cursorIndex = selection.anchor.character;
+        if (textEditor.document.lineAt(selection.anchor.line).text.length !== cursorIndex)
+            vscode.commands.executeCommand("cursorWordRight");
     });
     context.subscriptions.push(disposable);
 
-    disposable = vscode.commands.registerCommand('navigator.fixedSpaceLeft', () => {
-        Array.from(Array(10).keys()).forEach(() => vscode.commands.executeCommand("cursorLeft"));
+    disposable = vscode.commands.registerCommand('navigator.wordLeftBegin', () => {
+        const textEditor = vscode.window.activeTextEditor;
+        const selection = textEditor.selection;
+        const cursorIndex = selection.anchor.character;
+        if (cursorIndex !== 0)
+            vscode.commands.executeCommand("cursorWordLeft");
     });
     context.subscriptions.push(disposable);
 

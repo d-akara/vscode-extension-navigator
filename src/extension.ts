cfa5ee1f67e976137bc4add076ef857c4988ad00
Dakara <chad.meadows@gmail.com>
2018-01-05 17:32:42 -0500

    initial implementation of tree view

--- src/extension.ts
+++ src/extension.ts
@@ -4,6 +4,7 @@ import * as edit from 'vscode-extension-common'
 
 /**
  * - support custom tree views / palette lists
+ * - recent edit locations in tree view
  * - custom highlighting
  *   - duplicate lines or possibly show in palette and you can jump to them?
  * 
@@ -41,6 +42,42 @@ export function activate(context: vscode.ExtensionContext) {
     });
     context.subscriptions.push(disposable);
 
+    registerTreeDataProvider(context);
+
+}
+
+function registerTreeDataProvider(context: vscode.ExtensionContext) {
+    let selected;
+    const emitter = new vscode.EventEmitter<string | null>();
+    const provider = {
+        onDidChangeTreeData: emitter.event,
+        getChildren: element=> {
+            console.log('getting current selections')
+            return Promise.resolve(selected);
+        },
+        getTreeItem: element=> {
+            const selection = element as vscode.Selection;
+            return {
+                label: selection.anchor.line + ' - ' + vscode.window.activeTextEditor.document.lineAt(selection.anchor.line).text,
+                command: {title: 'reveal', command: 'revealLine', arguments: [{lineNumber:selection.anchor.line}]},
+                iconPath: {
+                    light: context.asAbsolutePath('icons/light/location.svg'),
+                    dark: context.asAbsolutePath('icons/dark/location.svg')
+                }
+            }
+        }
+    }
+    console.log('registering tree data')
+    let disposable = vscode.window.registerTreeDataProvider('navigation', provider)
+    context.subscriptions.push(disposable);
+    disposable = vscode.commands.registerCommand('navigator.view.save.matches', () => {
+        edit.matchesAsSelections(vscode.window.activeTextEditor)
+        .then(selections=> {
+           selected = selections;
+           emitter.fire()
+        })
+    });
+    context.subscriptions.push(disposable);    
 }
 
 export function deactivate() {
